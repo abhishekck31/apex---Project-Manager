98bc0640d4d83f6e70269eb37565fcca494d616f
Abhishek <abhishekkannur31@gmail.com>
2026-01-20 14:28:37 +0530

    feat: introduce mail utility for email verification and password reset templates

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -8,11 +8,32 @@ const emailVerificationMailGenContent = (user, verficationURL) => {
             action: {
                 instructions: "Please click on the button below to verify your email address.",
                 button: {
-                    color: '#3498db',
+                    color: 'rgba(52, 152, 219, 1)',
                     text: 'Verify Email Address',
                     link: verficationURL
-                }
-            }
-        }
-    }
-}
+                },
+            },
+            outro:
+                "Need help , or have questions ? Just reply to this email, we'd love to help you!"
+        },
+    };
+};
+
+const forgotPasswordMailGenContent = (user, passwordResetURL) => {
+    return {
+        body: {
+            name: username,
+            intro: "You have requested to reset your password.",
+            action: {
+                instructions: "Please click on the button below to reset your password.",
+                button: {
+                    color: 'rgba(52, 152, 219, 1)',
+                    text: 'Reset Password',
+                    link: passwordResetURL
+                },
+            },
+            outro:
+                "Need help , or have questions ? Just reply to this email, we'd love to help you!"
+        },
+    };
+};
